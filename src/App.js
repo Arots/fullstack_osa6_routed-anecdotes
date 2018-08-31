1a910d4fa99a96231ade7c40bd052f86b3f0a0fb
Aro <otso.aro@cgi.com>
2018-08-31 14:23:08 +0300

    6.22 asti tehty

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from 'react'
 import './styles.css'
-import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink, Redirect, Link } from 'react-router-dom'
+import { Button, FormControl, FormGroup, ControlLabel, PageHeader,
+   Alert, Navbar, Nav, NavItem, ListGroup, ListGroupItem, Grid, Row, Col, Image } from 'react-bootstrap'
+import AlanTuring from './AlanTuring.PNG'
 
 const Menu = () => (
   <div>
-    <div style={{backgroundColor: 'transparent', border: '2px dotted', borderRadius: '12px', padding: '15px' }}>
-      <NavLink 
-         exact to='/'
-        activeStyle={{
-          fontWeight: 'bold',
-          backgroundColor: 'pink',
-          padding: '10px',
-          border: '2px dashed',
-          borderRadius: '15px'
-        }}>
-        anecdotes</NavLink>&nbsp;
-      <NavLink 
-        exact to='/list'
-        activeStyle={{
-          fontWeight: 'bold',
-          backgroundColor: 'pink',
-          padding: '10px',
-          border: '2px dashed',
-          borderRadius: '15px'
-        }}>
-        create new</NavLink>&nbsp;
-      <NavLink 
-        exact to='/info' 
-        activeStyle={{
-          fontWeight: 'bold',
-          backgroundColor: 'pink',
-          padding: '10px',
-          border: '2px dashed',
-          borderRadius: '15px'
-        }}>
-        about</NavLink>&nbsp;
+    <div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            Software anecdotes
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem href="#">
+              <Link to="/">home</Link>
+            </NavItem>
+            <NavItem href="#">
+              <Link to="/list">create new</Link>
+            </NavItem>
+            <NavItem href="#">
+              <Link to="/info">about</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
     <br />
   </div>
@@ -42,18 +36,18 @@ const Menu = () => (
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >
-      <NavLink to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</NavLink></li>)}
-    </ul>  
+    <PageHeader><small>Anecdotes</small></PageHeader>
+    <ListGroup striped>
+        {anecdotes.map(anecdote => <ListGroupItem key={anecdote.id} >
+          <NavLink to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</NavLink> </ListGroupItem>)}
+    </ListGroup>
   </div>
 )
 
 const Anecdote = ({anecdote}) => {
   return (
     <div>
-      <h2>{anecdote.content} by {anecdote.author}</h2>
+      <PageHeader><small>{anecdote.content} by {anecdote.author}</small></PageHeader>
       <div>has {anecdote.votes} votes</div>
       <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div>
       <br/>
@@ -64,14 +58,27 @@ const Anecdote = ({anecdote}) => {
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+    <Grid>
+      <Row>
+        <Col xs={6} md={4} >
+        <p>According to Wikipedia:</p>
+        <em>An anecdote is a brief, revealing account of an individual person or an incident. 
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
       An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src={AlanTuring} circle />
+        </Col>
+        </Row>
+        <br/>
+        <Row>
+        <Col xs={12} md={8}>
+          Software engineering is full of excellent anecdotes, at this app you can find the best and add more.
+        </Col>
+      </Row>
+      <br/>
+    </Grid>
   </div>
 )
 
@@ -85,18 +92,12 @@ const Footer = () => (
 
 const Notification = ({notification}) => {
   const footerStyle = {
-    color: 'green',
-    fontStyle: 'Arial',
-    fontSize: 14,
-    border: '2px solid',
-    borderRadius: '15px 50px',
-    display: notification === '' ? 'none' : '',
-    padding: '8px'
+    display: notification === '' ? 'none' : ''
   }
   return (
-    <div style={footerStyle} >
-      {notification}
-    </div>
+    <Alert bsStyle="warning" style={footerStyle} >
+      <strong>{notification}</strong>
+    </Alert>
   )
 }
 
@@ -134,21 +135,17 @@ class CreateNew extends React.Component {
   render() {
     return(
       <div>
-        <h2>create a new anecdote</h2>
+        <PageHeader>create a new anecdote</PageHeader>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            content: 
-            <input className="input" name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author:
-            <input className="input" name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info:
-            <input className="input" name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
-          <button>create</button>
+        <FormGroup>
+          <ControlLabel>content: </ControlLabel>
+            <FormControl className="input" name='content' value={this.state.content} onChange={this.handleChange} />
+          <ControlLabel>author: </ControlLabel>
+            <FormControl className="input" name='author' value={this.state.author} onChange={this.handleChange} />
+          <ControlLabel>url for more info: </ControlLabel>
+            <FormControl className="input" name='info' value={this.state.info} onChange={this.handleChange} />
+          <Button bsStyle="success" type="submit">create</Button>
+        </FormGroup>
         </form>
       </div>  
     )
@@ -214,19 +211,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Software anecdotes</h1>
-        <div />
+      
+      <div className="container">
         <Router>
           <div>
-          <Menu />
+            <Menu />
             <Notification notification={this.state.notification} />
             <Route exact path='/' render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route exact path='/list' render={() => this.state.redirect ?
-              <Redirect to='/' /> : <CreateNew addNew={this.addNew} />}/>
-            <Route exact path='/info' render={() => <About />}/>
-            <Route exact path='/anecdotes/:id' render={({match}) => 
-              <Anecdote anecdote={this.anecdoteById(match.params.id)}/> }
+              <Redirect to='/' /> : <CreateNew addNew={this.addNew} />} />
+            <Route exact path='/info' render={() => <About />} />
+            <Route exact path='/anecdotes/:id' render={({ match }) =>
+              <Anecdote anecdote={this.anecdoteById(match.params.id)} />}
             />
           </div>
         </Router>
